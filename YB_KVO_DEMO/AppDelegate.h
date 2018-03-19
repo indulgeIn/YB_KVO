bf9ded92a85185c41bd4880a698b701cfac92888
杨少 <yangshao@yangshaodemac-mini.local>
2018-03-19 10:53:07 +0800

    d

---
+++ YB_KVO_DEMO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YB_KVO_DEMO
+//
+//  Created by 杨少 on 2018/3/19.
+//  Copyright © 2018年 杨波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YB_KVO_DEMO/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  YB_KVO_DEMO
+//
+//  Created by 杨少 on 2018/3/19.
+//  Copyright © 2018年 杨波. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+@end
